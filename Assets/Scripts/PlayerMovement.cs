461e71bd325e33436828185377859a2468979f0a
CreeperPlay86 <102041933+CreeperPlay86@users.noreply.github.com>
2022-10-09 19:30:13 +0500

    Add UI

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,41 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velosity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velosity.y < 0)
+        {
+            velosity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velosity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velosity.y += gravity * Time.deltaTime;
+
+        controller.Move(velosity * Time.deltaTime);
     }
 }
