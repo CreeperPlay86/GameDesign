812890483c7ac4860e08824cfabc4f2cdf343fe0
omletccblpom <tima.ledyaev@mail.ru>
2022-10-24 10:30:16 +0500

    Add Weapon Switching

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,24 +6,51 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
     
 
     public ParticleSystem muzzleFlash;
-
     public Camera fpsCam;
+
+    private float nextTimeToFire = 0f;
+
+    void Start()
+    {
+        if (currentAmmo == -1)
+            currentAmmo = maxAmmo;   
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (currentAmmo <= -1)
         {
+            Reload();
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
+    void Reload()
+    {
+        Debug.Log("Reloading...");
+        currentAmmo = maxAmmo;
+    }
+                      
     void Shoot()
     {
         muzzleFlash.Play();
+
+        currentAmmo--;
 
         RaycastHit hit;
         if (Physics.Raycast(muzzleFlash.transform.position, fpsCam.transform.forward, out hit, range))
